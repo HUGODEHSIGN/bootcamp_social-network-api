352df0ac907de9bc335317ce0a8a472b7d8080cc
Hugo Hsi <hhsi@hugodehsign.com>
2024-04-25 18:25:38 -0400

    added virtuals: true to user schema

---
+++ models/Thoughts.js
@@ -0,0 +1,6 @@
+import mongoose from 'mongoose';
+
+const thoughtSchema = new mongoose.Schema({
+  thoughtText: { type: String, required: true, minLength: 1, maxLength: 280 },
+  createdAt: { type: Date, default: Date.now },
+});
--- models/User.js
+++ models/User.js
@@ -1,19 +1,27 @@
 import mongoose from 'mongoose';
 
-const userSchema = new mongoose.Schema({
-  title: { type: String, unique: true, required: true, trim: true },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    match: [
-      /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
-      'Please provide a valid email address',
-    ],
+const userSchema = new mongoose.Schema(
+  {
+    title: { type: String, unique: true, required: true, trim: true },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [
+        /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
+        'Please provide a valid email address',
+      ],
+    },
+    thoughts: [{ _id: Number }],
+    friends: [{ _id: Number }],
   },
-  thoughts: [{ _id: Number }],
-  friends: [{ _id: Number }],
-});
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
 
 userSchema.virtual('friendCount').get(function () {
   return this.friends.length;
